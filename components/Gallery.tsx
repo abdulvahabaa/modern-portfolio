1b11ac0ef100881db467a92eaa19a03e638922ca
abdulvahabaa <abdulvahabaa916@gmail.com>
2025-08-22 10:06:46 +0530

    refactor:gallery file add

---
+++ components/Gallery.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div className="py-20 w-full" id="gallarly">
+      <h1 className="heading text-center">
+        My <span className="text-purple">Art Gallery</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Gallery;
